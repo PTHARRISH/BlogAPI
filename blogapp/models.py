705ff573a26e937c2dcebe916659f3f5e558e215
PTHARRISH <harrishpt@gmail.com>
2025-08-19 15:32:11 +0530

    first commit

---
+++ blogapp/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+# Create your models here.
+class CustomUser(AbstractUser):
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(
+        upload_to="profile_image", blank=True, null=True
+    )
+    facebook = models.URLField(max_length=255, blank=True, null=True)
+    yotube = models.URLField(max_length=255, blank=True, null=True)
+    twitter = models.URLField(max_length=255, blank=True, null=True)
+    instagram = models.URLField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
