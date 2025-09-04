0da64dd753695e72c5ccff2ee2d4c3e591b389bf
PTHARRISH <harrishpt@gmail.com>
2025-09-04 23:34:51 +0530

    Update blog

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path("register_users/", views.register_user, name="register_users"),
     path("create_blog/", views.create_blog, name="create_blog"),
+    path("blog_list/", views.blog_list, name="blog_list"),
+    path("update_blog/<int:pk>", views.update_blog, name="update_blog"),
 ]
